e640a87758ac1f5405482d669c3fad1f31cf230f
Geonhui Lee <133332277+kor060722@users.noreply.github.com>
2024-07-29 15:57:11 +0900

    Update ssm-set.sh

--- scripts-repo/ssm-set.sh
+++ scripts-repo/ssm-set.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo yum install -y amazon-ssm-agent
+sudo yum install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
 sudo systemctl enable --now amazon-ssm-agent
 sudo systemctl restart amazon-ssm-agent
